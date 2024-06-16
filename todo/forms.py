c38da76577b5968d41a2e14936455c82d8208779
Sima3443 <toppochka@gmail.com>
2024-06-17 00:33:38 +0300

    edit design

--- todo/forms.py
+++ todo/forms.py
@@ -9,11 +9,11 @@ class TodoForm(ModelForm):
         
     title = forms.CharField(widget=forms.TextInput(attrs={
         'class': 'w-full px-3 py-2 border rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500',
-        'placeholder': 'Введіть назву'
+        'placeholder': 'Enter a title'
     }))
     content = forms.CharField(widget=forms.Textarea(attrs={
         'class': 'w-full px-3 py-2 border rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500',
-        'placeholder': 'Введіть опис'
+        'placeholder': 'Enter discription'
     }))
     image = forms.ImageField(widget=forms.FileInput(attrs={
         'class': 'w-full px-3 py-2 border rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500'
--- users/forms.py
+++ users/forms.py
@@ -13,15 +13,15 @@ class SignUpForm(UserCreationForm):
 
     username = forms.CharField(max_length=254, widget=forms.TextInput(attrs={
         'class': 'form-control block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6',
-        'placeholder': 'Введите имя пользователя'
+        'placeholder': 'Your name'
     }))
     password1 = forms.CharField(widget=forms.PasswordInput(attrs={
         'class': 'form-control block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6',
-        'placeholder': 'Введите пароль'
+        'placeholder': 'Password'
     }))
     password2 = forms.CharField(widget=forms.PasswordInput(attrs={
         'class': 'form-control block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6',
-        'placeholder': 'Подтвердите пароль'
+        'placeholder': 'Confirm password'
     }))
 
 class LoginForm(AuthenticationForm):
